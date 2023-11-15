9381acb27f56352e134f28dfe58f65e3f3c2cad4
Davidenko Aleksey <davaleks82@mail.ru>
2023-11-15 19:18:10 +0300

    first commit

---
+++ seminar_1/exemple1.py
@@ -0,0 +1,13 @@
+# ввести 2 числа и определить какое из них больше
+
+first = int(input("first digit "))
+second = int(input("second digit "))
+
+if first > second:
+    print(f"first = {first} > second = {second}")
+
+elif first == second:
+    print(f"first = {first} == second = {second}")
+
+else:
+    print(f"first = {first} < second = {second}")            
