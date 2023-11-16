683fa7c73bc0bf340c176843f1083e7d9ad7f2bd
Davidenko Aleksey <davaleks82@mail.ru>
2023-11-16 16:21:42 +0300

    1 zadanie

--- seminar_1/exemple1.py
+++ seminar_1/exemple1.py
@@ -1,13 +1,29 @@
 # ввести 2 числа и определить какое из них больше
 
-first = int(input("first digit "))
-second = int(input("second digit "))
+# first = int(input("first digit "))
+# second = int(input("second digit "))
 
-if first > second:
-    print(f"first = {first} > second = {second}")
+# if first > second:
+#     print(f"first = {first} > second = {second}")
 
-elif first == second:
-    print(f"first = {first} == second = {second}")
+# elif first == second:
+#     print(f"first = {first} == second = {second}")
 
-else:
-    print(f"first = {first} < second = {second}")            
+# else:
+#     print(f"first = {first} < second = {second}")            
+
+
+# firstClass = int(input("Кол-во человек в первом классе "))
+# secondClass = int(input("Кол-во человек во втором классе "))
+# thirdClass = int(input("Кол-во человек в третьем классе "))
+
+# schoolDesk = (firstClass + secondClass + thirdClass) // 2
+
+# print(f"Кол-во парт = {schoolDesk}")
+
+n = int(input("Машина проезжает за день -  "))
+m = int(input("Длинна маршрута -  "))
+
+d = (m // -n)
+
+print(f"Проедет за {-d} дней")
